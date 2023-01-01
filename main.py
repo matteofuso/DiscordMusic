a97644bd73b5b89189b2541911ef47740d93395e
Matteo <matteo.fuso@iisviolamarchesini.edu.it>
2023-01-01 16:58:54 +0100

    Add Token storage

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import json
+import requests
+from colorama import Fore, Style
 import discord
 
+try:
+    with open("config.json", 'r') as f:
+        config = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    config = {}
+    print("Inserisci il token del bot:")
+
+while True:
+    if config.get("token") == None:
+        config["token"] = input("> ")
+    token = config["token"]
+    head = {
+        "Authorization": f"Bot {token}"
+    }
+    try:
+        data = requests.get("https://discord.com/api/v10/users/@me", headers=head).json()
+    except requests.exceptions.RequestException as e:
+        if e.__class__ == requests.exceptions.ConnectionError:
+            exit(f"{Fore.RED}ConnectionError{Fore.RESET}: Discord è bloccato nelle reti pubbliche, verifica di riuscire ad accedere a https://discord.com")
+
+        elif e.__class__ == requests.exceptions.Timeout:
+            exit(f"{Fore.RED}Timeout{Fore.RESET}: La connessione alle API di Discord ha impiegato troppo tempo (Sei rate limited?)")
+        exit(f"Errore sconosciuto! Ulteriori informazioni:\n{e}")
+    
+    if data.get("id") != None:
+        break
+    print(f"{Fore.RED}Token non valido{Fore.RESET} Reinserisci il token: ")
+    config.pop("token", None)
+
+with open("config.json", "w") as f:
+    f.write(json.dumps(config, indent=4))
+    
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -11,4 +46,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('Token')
+client.run(config["token"])
