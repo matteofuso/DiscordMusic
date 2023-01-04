29326b97f7bef39f38b0c4da3e432d6bc6175f87
Matteo <matteo.fuso@iisviolamarchesini.edu.it>
2023-01-04 19:46:51 +0100

    Add download + fix playlist

--- youtube.py
+++ youtube.py
@@ -1,17 +1,16 @@
 import json
 import requests
-import re 
+import re
+from pytube import YouTube
 
 try:
     with open("config.json", 'r') as f:
         config = json.load(f)
 except (FileNotFoundError, json.JSONDecodeError):
     config = {}
-
-# Replace YOUR_API_KEY with your actual API key
 api_key = config["youtube"]["key"]
 
-def yt_video(link):
+def video(link):
     video_url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={link}&key={api_key}"
     response = requests.get(video_url)
     video_data = response.json()
@@ -25,41 +24,67 @@ def yt_video(link):
     except IndexError:
         return False
 
-def yt_playlist(link):
-    video_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={link}&key={api_key}"
-    response = requests.get(video_url)
-    video_data = response.json()
+def playlist(link):
+    next = ""
+    remaining = ""
     videos = []
-    try:
-        data = video_data["items"]
-        data[0]
-        for video in data:
-            videos.append({
-            "name": video["snippet"]["title"],
-            "author": video["snippet"]["videoOwnerChannelTitle"],
-            "id": video["snippet"]["resourceId"]["videoId"]
-            })
-        return videos
-    except IndexError:
-        return False
+    while True:
+        video_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={link}&key={api_key}&maxResults=50{next}"
+        response = requests.get(video_url)
+        video_data = response.json()
+        try:
+            data = video_data["items"]
+            data[0]
+            for video in data:
+                videos.append({
+                "name": video["snippet"]["title"],
+                "author": video["snippet"]["videoOwnerChannelTitle"],
+                "id": video["snippet"]["resourceId"]["videoId"]
+                })
+            try:
+                pageToken = video_data["nextPageToken"]
+                next = f"&pageToken={pageToken}"
+                if remaining == "":
+                    remaining = video_data["pageInfo"]["totalResults"] - 50
+                else:
+                    remaining = remaining - 50
+            except KeyError:
+                remaining = 0
+        except IndexError:
+            return False
+        if remaining < 0:
+            break
+    return videos
 
-def get_youtube(string):
+def get(string):
     regex = r'^(https?://)?(www\.)?'
-    regex += r'(youtube|youtu|youtube-nocookie)\.(com|be)/(.*)'
+    regex += r'(music.youtube|youtube|youtu|youtube-nocookie)\.(com|be)/(.*)'
     match = re.search(regex, string)
     if match:
         link = (match.group(5))
         try:
             link = link.split("list=")[1].split("&")[0]
-            return yt_playlist(link)
+            return playlist(link)
         except IndexError:
             if link.startswith("watch?"):
                 link = link.split("&")[0]
                 link = link.replace("watch?v=", "")
             else:
                 link = link.split("?")[0]
-        return yt_video(link)
+        return video(link)
     else:
         return False
     
-print(json.dumps(get_youtube("https://www.youtube.com/watch?v=8Yg1FnKE66I&list=PLFSjo0XzsV12BG03Y2l7ojhVBjyPsg9HZ&index=3&t=16s"), indent=4))
+def download_id(id):
+    video = YouTube(f"https://www.youtube.com/watch?v={id}")
+    video.streams.get_audio_only().download(f"./cache/", filename=f"youtube_{id}.mp3")
+        
+def download_link(link):
+    ids = get(link)
+    if ids != False:
+        try:
+            ids[1]
+        except KeyError:
+            download_id(ids["id"])
+    
+download_link("https://music.youtube.com/watch?v=4ywb2pXRYZI&feature=share")
